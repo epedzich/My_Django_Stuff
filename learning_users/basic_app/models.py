8cce586bdc36cbbeb826f53e4d157c74028e64cf
Ewelina <moquedire@yahoo.com>
2017-11-16 14:25:23 +0100

    update

--- learning_users/basic_app/models.py
+++ learning_users/basic_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-import django.contrib.auth.models import User
+from django.contrib.auth.models import User
 
 # Create your models here.
 class UserProfileInfo(models.Model):
